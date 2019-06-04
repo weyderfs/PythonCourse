e6e811ae5719c5de89816ddb6ee0e69a195b508e
Azahast <weyder.santos@br.experian.com>
2019-06-04 12:10:35 -0300

    Add new alternative method, rounding without import math

--- Desafios/desafio16.py
+++ Desafios/desafio16.py
@@ -1,5 +1,11 @@
 #import math
+###Adding perfomance import only library necessarie
 from math import trunc
 
 n = float(input('Insira um número real: '))
 print('O número informado foi: {} e o seu inteiro é {}'.format(n, trunc(n)))
+
+#####Without import
+'''n = float(input('Insira um número real: '))
+print('O número informado foi: {} e o seu inteiro é {}'.format(n, int(n)))'''
+
