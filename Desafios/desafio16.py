a4d36d6b930d620e50f24ebaba73bc1dd51c38e4
Azahast <weyder.santos@br.experian.com>
2019-06-04 12:07:18 -0300

    adding perfomance on code importing only function necessarie

--- Desafios/desafio16.py
+++ Desafios/desafio16.py
@@ -1,4 +1,5 @@
-import math
+#import math
+from math import trunc
 
 n = float(input('Insira um número real: '))
-print('O número informado foi: {} e o seu inteiro é {}'.format(n, math.trunc(n)))
+print('O número informado foi: {} e o seu inteiro é {}'.format(n, trunc(n)))
