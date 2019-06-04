99ad7760386512053d670d0c750680eab25efd26
Azahast <weyder.santos@br.experian.com>
2019-06-04 12:04:37 -0300

    fix library in use to math.trunk()

--- Desafios/desafio16.py
+++ Desafios/desafio16.py
@@ -1,4 +1,4 @@
 import math
 
 n = float(input('Insira um número real: '))
-print('O número informado foi: {} e o seu inteiro é {}'.format(n, math.floor(n)))
+print('O número informado foi: {} e o seu inteiro é {}'.format(n, math.trunc(n)))
