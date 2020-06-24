344ed97b04f43c7dd35127154437d209f8c76310
weyderfs <weyderfs@gmail.com>
2020-06-23 21:17:08 -0300

    hotfix/develop - fix print in the script

--- Desafios/desafio02_aula04.py
+++ Desafios/desafio02_aula04.py
@@ -1,6 +1,6 @@
-nome = input('Qual seu nome?: ')
-dn = input('Que dia você nasceu?: ')
-dm = input('Qual mês você nasceu?: ')
-da = input('Que ano você nasceu?: ')
+nome = input ('Qual seu nome?: ')
+dn = input ('Que dia você nasceu?: ')
+dm = input ('Qual mês você nasceu?: ')
+da = input ('Que ano você nasceu?: ')
 
-print = ('Olá'+ nome, 'você nasdeu dia:'+ dn, 'mês: '+ dm, 'e no ano: '+ da)
+print ('Olá', nome, 'você nasdeu dia:', dn,'mês:', dm, 'e no ano:', da)
