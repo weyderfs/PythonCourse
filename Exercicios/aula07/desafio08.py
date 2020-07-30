b2cdca4eafbc1ee9ab9312c2606e3a7fb00a80ea
Azahast <weyderfs@gmail.com>
2020-07-29 22:37:55 -0300

    feature/develop - saving challenger 008

---
+++ Exercicios/aula07/desafio08.py
@@ -0,0 +1,3 @@
+n = float(input('Input the measure in meters: '))
+print('There distance in centimeters is: {:.1f} cm'.format(n*100))
+print('There distance in milimeters is: {:.1f} mm'.format(n*1000))
