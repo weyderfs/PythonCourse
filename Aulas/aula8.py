3cf47dd21cccc1ca902efdf071085fd8728b2586
Azahast <weyderfs@gmail.com>
2020-08-24 21:51:03 -0300

    feature/develop - Class eight

---
+++ Aulas/aula8.py
@@ -0,0 +1,7 @@
+import math
+
+n = int(input('Entry with a number: '))
+
+r = math.sqrt(n)
+
+print('The Square root of you number is: {:.2f}'.format(r))
---
+++ Aulas/aula8a.py
@@ -0,0 +1,7 @@
+import random
+
+n = random.random()
+print(n)
+
+a = random.randint(1, 100)
+print(a)
