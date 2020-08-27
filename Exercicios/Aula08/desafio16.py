9c2762e0e96cd6c79ce25fc9411c33f4ac44a66f
Azahast <weyderfs@gmail.com>
2020-08-27 16:37:49 -0300

    feature/develop - Challenger 18

---
+++ Exercicios/Aula08/desafio16.py
@@ -0,0 +1,5 @@
+#import math
+from math import trunc
+n = float(input("Input the number: "))
+#print("You post the number {} and the Integer number is: {}".format(n, math.trunc(n)))
+print("You post the number {} and the Integer number is: {}".format(n, trunc(n)))
