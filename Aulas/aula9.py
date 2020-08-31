fcf1d4cecb857ffc589bb104a774389f20ec5c73
Azahast <weyderfs@gmail.com>
2020-08-31 15:54:19 -0300

    feature/develop - Continuos learning

---
+++ Aulas/aula9.py
@@ -0,0 +1,36 @@
+frase = 'Curso em Vídeo Python'
+print(frase[1:15])
+print(frase[3])
+print(frase[3:13])
+print(frase[:13])
+print(frase[1:15:2])
+print(frase[1::2])
+print(frase[::2])
+
+print("")
+
+print("""Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.""")
+
+print(" ")
+
+print(frase.count('o'))
+print(frase.count('O'))
+print(frase.upper().count('O'))
+print(len(frase))
+print(frase.replace('Python', 'Android'))
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.find('video'))
+print(frase.split())
+
+print(" ")
+
+divido = frase.split()
+print(divido[0])
+print(divido[2][3])
+
+print(" ")
+
+#https://www.youtube.com/watch?v=a7DH88vk2Sk&feature=youtu.be
+print(" Link da Aula: https://www.youtube.com/watch?v=a7DH88vk2Sk&feature=youtu.be")
+
