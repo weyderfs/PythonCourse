9fdee3d1402a5e87841e51fd50cefdcbd83d6693
Azahast <weyderfs@gmail.com>
2020-09-08 13:37:49 -0300

    feature/develop - Challenger 24

---
+++ Exercicios/Aula09/desafio24.py
@@ -0,0 +1,5 @@
+c = str(input('Em que cidade você nasceu? ')).strip()
+print(c[:5].upper() == 'SANTO')
+
+## .strip() - Remove os espaços inseridos no inicio e final da string do Input
+## .upper() - Transforma em CAPS LOCK
