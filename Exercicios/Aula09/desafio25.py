102f3979ad0dfe0be8b9e81722e82a8703655b06
Azahast <weyderfs@gmail.com>
2020-09-08 13:46:13 -0300

    feature/develop - Challenger 25

---
+++ Exercicios/Aula09/desafio25.py
@@ -0,0 +1,4 @@
+n = str(input('Insira o nome: ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in n.lower())) # OU .format('Silva' in n.upper()))
+
+## Relembrando que o 'in' é um operador no Python
