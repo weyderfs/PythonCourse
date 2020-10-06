f5658b79ca1e6bdec6240c61c2da7ac4e7581a1d
Azahast <weyderfs@gmail.com>
2020-10-06 16:51:30 -0300

    feature/develop - Challenger 32

---
+++ Exercicios/Aula10/desafio32.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+a = int(input('Qual ano deseja verificar? Se quiser o ano atual digite 0: '))
+
+if a == 0:
+    a = date.today().year
+    
+if a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
+    print('o Ano {} é BISSEXTO'.format(a))
+else:
+    print('o Ano {} NÃO é BISSEXTO'.format(a))
