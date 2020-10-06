a59ef9f2f319664fcb06c372b8e0c3350e1cb9fc
Azahast <weyderfs@gmail.com>
2020-10-06 16:52:39 -0300

    feature/develop - Challenger 32

--- Exercicios/Aula10/desafio32.py
+++ Exercicios/Aula10/desafio32.py
@@ -4,7 +4,7 @@ a = int(input('Qual ano deseja verificar? Se quiser o ano atual digite 0: '))
 
 if a == 0:
     a = date.today().year
-    
+
 if a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
     print('o Ano {} é BISSEXTO'.format(a))
 else:
